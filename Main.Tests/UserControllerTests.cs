83d5e2f9fd0dca8cb0aec37b79664d7ec3826899
Mihai Stancu <mihaistancu@mail.com>
2021-03-23 23:45:00 +0200

    Adds unit test

--- Main.Tests/UserControllerTests.cs
+++ Main.Tests/UserControllerTests.cs
@@ -13,6 +13,40 @@ namespace Main.Tests
     public class UsersControllerTests
     {
         [Fact]
+        public async void RegisterNewUser()
+        {
+            // When checking if the user exists already, return null (doesn't exist)
+            var mockRepository = new Mock<IUserRepository>();
+            mockRepository.Setup(x => x.GetByUsername("username"))
+                .Returns(Task.FromResult<UserItem>(null));
+
+            // When writing any UserItem to db, return this UserItem
+            var newUserItem = new UserItem();
+            newUserItem.FirstName = "firstName";
+            newUserItem.LastName = "lastName";
+            newUserItem.Username = "username";
+            newUserItem.Role = Role.User; 
+            byte[] passwordHash, passwordSalt;
+            CreatePasswordHash("password", out passwordHash, out passwordSalt);
+            newUserItem.PasswordHash =  passwordHash;
+            newUserItem.PasswordSalt = passwordSalt;
+            mockRepository.Setup(x => x.Create(It.IsAny<UserItem>()))
+                .Returns(Task.FromResult<UserItem>(newUserItem));
+
+            var controller = new UsersController(mockRepository.Object);
+
+            // Act
+            UserRegisterRequest request = new UserRegisterRequest();
+            request.Username =  "username";
+            request.Password =  "password";
+            request.FirstName = "firstName";
+            request.FirstName = "lastName";
+
+            IActionResult actionResult = await controller.Register(request);
+            Assert.IsType<OkObjectResult>(actionResult);
+        }
+
+        [Fact]
         public async void AuthenticateWrongCredentials()
         {
             // Arrange
