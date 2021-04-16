970c2edbd8d85c7708a67bb6beef6ba4baf183e1
Mihai Stancu <mihaistancu@mail.com>
2021-04-16 23:54:46 +0300

    Minor tweak

--- Main/Controllers/UserController.cs
+++ Main/Controllers/UserController.cs
@@ -51,7 +51,7 @@ namespace Main.Controllers
             {
                 Subject = new ClaimsIdentity(new Claim[]
                 {
-                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                    new Claim(ClaimTypes.Name, user.Id)
                 }),
                 Expires = DateTime.UtcNow.AddDays(7),
                 SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
